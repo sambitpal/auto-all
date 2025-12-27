93a40774e95f7d28b3b56830ca22249f004d2d7b
sambitpal <mailm.sambitpal@gmail.com>
2025-12-27 16:14:01 +0530

    Added UI tests

---
+++ restAssuredWithMockAPI/src/test/java/uiTests/SearchProductTests.java
@@ -0,0 +1,14 @@
+package uiTests;
+
+import org.testng.annotations.Test;
+
+import setup.BaseTestUI;
+
+public class SearchProductTests extends BaseTestUI{
+	
+	@Test
+	public void SearchForItem()
+	{
+	}
+
+}
